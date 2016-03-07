5f8a1b1d314948418704399344519a07aa494944
Tetiana Platonova <tetiana.platonova@kreuzwerker.pl>
2016-03-07 15:40:22 +0100

    Add styles

--- script/main.js
+++ script/main.js
@@ -1,6 +1,22 @@
 /**
  * Created by tati on 05.03.16.
  */
+
+var ColorPicker = React.createClass({
+    render: function (){
+        return (
+            <div className="color-picker">
+                <div color="0"></div>
+                <div color="1"></div>
+                <div color="2"></div>
+                <div color="3"></div>
+                <div color="4"></div>
+                <div color="5"></div>
+            </div>
+        );
+    }
+});
+
 var PlayerField = React.createClass({
     render: function(){
         var arr = [];
@@ -19,14 +35,10 @@ var HintField = React.createClass({
     render: function(){
         return (
             <div className="hint">
-                <div>
-                    <div className="little-circle"></div>
-                    <div className="little-circle"></div>
-                </div>
-                <div>
-                    <div className="little-circle"></div>
-                    <div className="little-circle"></div>
-                </div>
+                <div className="little-circle"></div>
+                <div className="little-circle"></div>
+                <div className="little-circle"></div>
+                <div className="little-circle"></div>
             </div>
         );
     }
@@ -45,11 +57,17 @@ var Row = React.createClass({
 
 var Game = React.createClass({
     render: function(){
-    return(
-        <div>
-            <Row />
-        </div>
-    );
+        var rows = [];
+        for (var i = 0; i < 12; i++){
+            rows.push(<Row key={i}/>);
+        }
+        return (
+            <div>
+                {rows}
+                <ColorPicker />
+            </div>
+
+        );
 }
 });
 
--- server.js
+++ server.js
@@ -5,3 +5,4 @@ var express = require('express');
 var server = express();
 server.use('/', express.static(__dirname + '/'));
 server.listen(8080, '127.0.0.1');
+console.log('Listening on the port 8080...');
